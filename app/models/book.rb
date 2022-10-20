963e03c810671846175793e7c44ace2854ccec89
Alejandro Ramos <arencinosa@gmail.com>
2022-10-19 23:59:15 -0400

    [bugfix] Adding missing relationship on books and bookstores

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
     validates :name, :presence => true
+
+    has_many :stocks, :dependent => :destroy
+    has_many :bookstores, :through => :stocks
 end
--- app/models/bookstore.rb
+++ app/models/bookstore.rb
@@ -1,3 +1,6 @@
 class Bookstore < ApplicationRecord
     validates :name, :presence => true
+
+    has_many :stocks, :dependent => :destroy
+    has_many :books, :through => :stocks
 end
